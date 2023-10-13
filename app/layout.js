09cf6e735c1d2a1e9351cfd282dc7af540b6dd5c
Dann Wee <dannweezj@gmail.com>
2023-10-13 21:04:04 +0700

    header nav bar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Provider from './Provider'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import HeaderNavBar from '../components/HeaderNavBar.js'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Provider>
+          <HeaderNavBar />
           {children}
         </Provider>
       </body>
