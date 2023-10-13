b1fe1143f8ccf5c646195d9033c97498150de43f
Dann Wee <dannweezj@gmail.com>
2023-10-13 20:24:40 +0700

    authentication sign-in sign-out

--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
+"use client"
+
+import { signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const {data:session} = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if(!session?.user) {
+      router.push("/sign-in");
+    }
+  },[session])
+
   return (
     <div>
-      
+      Home Page
+      <button onClick={() => signOut()}>Sign Out</button>
     </div>
   )
 }
