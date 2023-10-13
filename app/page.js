3211a3d9e973a376250b75c1672005ea84d78105
Dann Wee <dannweezj@gmail.com>
2023-10-13 21:09:06 +0700

    segment site

--- app/page.js
+++ app/page.js
@@ -15,8 +15,13 @@ export default function Home() {
   },[session])
 
   return (
-    <div>
-      Home Page
+    <div className="grid grid-cols-4 h-screen">
+      <div className="bg-blue-300">
+        First
+      </div>
+      <div className="bg-green-300 col-span-3">
+        Second
+      </div>
       <button onClick={() => signOut()}>Sign Out</button>
     </div>
   )
