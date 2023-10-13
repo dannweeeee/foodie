985a6679c94d7c92750eabb889dbccca7c8a06c2
Dann Wee <dannweezj@gmail.com>
2023-10-14 01:04:53 +0700

    added map styling

--- components/Home/GoogleMapView.js
+++ components/Home/GoogleMapView.js
@@ -2,7 +2,7 @@ import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 function GoogleMapView() {
   const { isLoaded } = useJsApiLoader({
-    id: ['ec7be0fc20bd33e8'],
+    id: ['google-map-script'],
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
   });
   const coordinate = {lat:-34.397, lng:150.644}
@@ -16,7 +16,7 @@ function GoogleMapView() {
       mapContainerStyle={containerStyle}
       center={coordinate}
       zoom={12}
-      options={{id:'ec7be0fc20bd33e8'}}
+      options={{mapId: '4ccaceca683e75ce'}}
     >
     </GoogleMap>
   ) : (
