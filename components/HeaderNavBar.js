f2227feeaba08a5140bcb04e2c317bd191b85876
Dann Wee <dannweezj@gmail.com>
2023-10-14 14:27:47 +0700

    update ui

--- components/HeaderNavBar.js
+++ components/HeaderNavBar.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { signOut, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 
@@ -15,39 +15,28 @@ function HeaderNavBar() {
   },[profileClick==true])
 
   return session?.user&& (
-    <div className="flex items-center justify-between p-5 shadow-md">
+    <div className="flex items-center justify-between p-5 ">
       <div className="flex gap-7 items-center">
         <Image src="/grey-logo-no-bg.png" alt="logo" width={160} height={160}/>
         <h2>Home</h2>
         <h2>Favourites</h2>
       </div>
-      {/* <div className="bg-gray-100 p-[6px] rounded-md w-[40%] gap-3 hidden md:flex">
+      <div className="bg-gray-100 p-[6px] rounded-md w-[40%] gap-3 hidden md:flex">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
         </svg>
         <input type="text" placeholder="Search" className="bg-transparent outline-none w-full" />
-      </div> */}
+      </div>
       <div className="flex gap-5">
-      {session?.user ? (
-          <>
-            <Image
-              src={session.user.image}
-              alt="user"
-              width={40}
-              height={40}
-              onClick={()=>setProfileClick(!profileClick)}
-              className="rounded-full cursor-pointer 
-              hover:border-[2px] border-blue-500"
-            />
-           {profileClick? 
-              <div className="absolute bg-white p-3 shadow-md border-[1px] mt-12 z-30 right-4">
-                <h2 className="cursor-pointer hover:text-blue-500 hover:font-bold"onClick={()=>signOut()}>
-                  Sign Out
-                </h2>
-              </div>:null
-            }
-          </>
-        ) : null}
+        {session?.user ? 
+          <Image src={session.user.image} 
+            alt='user' 
+            width={40} 
+            height={40}
+            className="rounded-full"
+          /> : null
+        } 
+        <button onClick={() => signOut()}>Sign Out</button>
       </div>
     </div>
   )
