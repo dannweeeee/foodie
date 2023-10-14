d186db7df0748376e0e28eecd32eca6d5ed57eb1
Dann Wee <dannweezj@gmail.com>
2023-10-14 10:39:45 +0700

    user location

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,44 @@
+"use client"
+
 import Provider from './Provider'
 import './globals.css'
 import { Raleway } from 'next/font/google'
 import HeaderNavBar from '../components/HeaderNavBar.js'
+import { useEffect, useState } from 'react'
+import { UserLocationContext } from '../context/UserLocationContext.js'
 
 const raleway = Raleway({ subsets: ['latin'] })
 
-export const metadata = {
+const metadata = {
   title: 'Foodie',
-  description: 'Restaurant-Finder Tool',
+  description: 'Foodie - A Restaurant-Finder Tool by Dann',
 }
 
 export default function RootLayout({ children }) {
+  const [userLocation, setUserLocation] = useState([]);
+
+  useEffect(() => {
+    getUserLocation();
+  },[])
+
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position)
+      setUserLocation({
+        lat:position.coords.latitude,
+        lng:position.coords.longitude
+      })
+    })
+  }
+
   return (
     <html lang="en">
       <body className={raleway.className}>
         <Provider>
-          <HeaderNavBar />
-          {children}
+          <UserLocationContext.Provider value={{userLocation, setUserLocation}}>
+            <HeaderNavBar />
+            {children}
+          </UserLocationContext.Provider>
         </Provider>
       </body>
     </html>
--- components/Home/GoogleMapView.js
+++ components/Home/GoogleMapView.js
@@ -1,11 +1,15 @@
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
+import { useContext } from 'react';
+import { UserLocationContext } from '../../context/UserLocationContext';
 
 function GoogleMapView() {
+  const {userLocation, setUserLocation} = useContext(UserLocationContext);
   const { isLoaded } = useJsApiLoader({
     id: ['google-map-script'],
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
   });
   const coordinate = {lat:-34.397, lng:150.644}
+  console.log(userLocation);
   const containerStyle = {
     width: '100%',
     height: '500px',
@@ -14,10 +18,20 @@ function GoogleMapView() {
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
-      center={coordinate}
+      center={userLocation}
       zoom={12}
       options={{mapId: '4ccaceca683e75ce'}}
     >
+      <MarkerF 
+        position={userLocation}
+        icon={{
+          url:'/user-location.png',
+          scaledSize: {
+            width: 50,
+            height: 50,
+          }
+        }}
+      />
     </GoogleMap>
   ) : (
     <div>Loading...</div>
